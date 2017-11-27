d58d4a130338d745992325df51b2e072a8b75c6d
Pavel Shackih <pavel.shackih@gmail.com>
2017-11-27 12:29:18 +0300

    fix push show

--- app/src/main/java/io/pavelshackih/firebasesample/MainActivity.kt
+++ app/src/main/java/io/pavelshackih/firebasesample/MainActivity.kt
@@ -3,12 +3,19 @@ package io.pavelshackih.firebasesample
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.iid.FirebaseInstanceId
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,5 +34,35 @@ class MainActivity : AppCompatActivity() {
             clipboard.primaryClip = clip
             Toast.makeText(applicationContext, "Token copied", Toast.LENGTH_LONG).show()
         }
+
+        findViewById<Button>(R.id.send_button).setOnClickListener {
+            token?.let {
+                SendTokenTask(it).execute()
+            }
+        }
+    }
+
+    class SendTokenTask(private val token: String) : AsyncTask<Unit, Unit, Unit>() {
+
+        override fun doInBackground(vararg params: Unit?) {
+            val client = OkHttpClient().newBuilder().build()
+            val requestBody = MultipartBody.Builder()
+                    .addFormDataPart("name", Build.MODEL)
+                    .addFormDataPart("token", token)
+                    .addFormDataPart("system", Build.VERSION.RELEASE)
+                    .build()
+
+            val request = Request.Builder()
+                    .url("http://push-ping.us-west-2.elasticbeanstalk.com/save")
+                    .post(requestBody)
+                    .build()
+
+            val response = client.newCall(request).execute()
+            Log.d(TAG, "Response : $response")
+        }
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
--- app/src/main/java/io/pavelshackih/firebasesample/MyFirebaseMessagingService.kt
+++ app/src/main/java/io/pavelshackih/firebasesample/MyFirebaseMessagingService.kt
@@ -1,13 +1,66 @@
 package io.pavelshackih.firebasesample
 
-import android.util.Log
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
+import android.support.v4.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.util.concurrent.atomic.AtomicInteger
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage?) {
         super.onMessageReceived(message)
-        Log.d("PUSH", "New push: $message")
+        message?.notification?.body?.let {
+            sendNotification("Application push", it)
+        }
+    }
+
+    private fun sendNotification(title: String, message: String) {
+        val intent = Intent(this, MainActivity::class.java)
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, "default")
+                .setSmallIcon(R.drawable.ic_sentiment_very_satisfied_black_24dp)
+                .setLargeIcon(BitmapFactory.decodeResource(this.resources,
+                        R.mipmap.ic_launcher))
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            val id = "default"
+            val name = "default"
+            val description = "default"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(id, name, importance)
+            mChannel.description = description
+            mChannel.enableLights(true)
+            mChannel.lightColor = Color.RED
+            mChannel.enableVibration(true)
+            mChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        notificationManager.notify(counter.incrementAndGet(), notificationBuilder.build())
+    }
+
+    companion object {
+        val counter = AtomicInteger(0)
     }
 }
